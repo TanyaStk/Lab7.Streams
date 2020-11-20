306b96b4ed58a570aa8d4897f2a0df99a744fcf4
TanyaStk <64659217+TanyaStk@users.noreply.github.com>
2020-11-20 13:54:10 +0300

    Develop

--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -3,37 +3,37 @@ package com.project;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
-public class Main extends Thread {
-    private final int size;
-    private SortingType sortingType;
-
-    public Main(int size, int sortingType) {
-        this.size = size;
-        switch (sortingType) {
-            case 1 -> this.sortingType = SortingType.INCREASING_BY_VALUE;
-            case 2 -> this.sortingType = SortingType.DECREASING_BY_VALUE;
-            case 3 -> this.sortingType = SortingType.INCREASING_BY_LENGTH;
-            case 4 -> this.sortingType = SortingType.DECREASING_BY_LENGTH;
-        }
-    }
-
-    @Override
-    public synchronized void start() {
-        List<Integer> array = new ArrayList<>();
-        Random random = new Random();
-        for (int i = 0; i < size; i++) {
-            array.add(random.nextInt(1000));
-        }
-        System.out.println("Original array:");
-        for (int elem : array) {
-            System.out.print(elem + " ");
-        }
-        Sort sort = new Sort(sortingType);
-        sort.run(array);
-        System.out.println("Sorted array:");
-        for (int elem : array) {
-            System.out.print(elem + " ");
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter array dimension:");
+            int size = scanner.nextInt();
+            List<Integer> array = new ArrayList<>();
+            Random random = new Random();
+            for (int i = 0; i < size; i++) {
+                array.add(random.nextInt(1000));
+            }
+            System.out.println("Original array:");
+            for (int elem : array) {
+                System.out.print(elem + " ");
+            }
+            System.out.println("\nEnter sorting type:\n" +
+                    "1 - increasing by value\n" +
+                    "2 - decreasing by value\n" +
+                    "3 - increasing by length\n" +
+                    "4 - decreasing by length.");
+            int sortingType = scanner.nextInt();
+            Sort sort = new Sort(sortingType, array);
+            sort.join();
+            sort.start();
+            System.out.println("Sorted array:");
+            for (int elem : array) {
+                System.out.print(elem + " ");
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
         }
     }
 }
--- src/main/java/com/project/Sort.java
+++ src/main/java/com/project/Sort.java
@@ -12,18 +12,26 @@ enum SortingType {
 }
 
 public class Sort extends Thread {
-    private final SortingType sortingType;
+    private SortingType sortingType;
+    private final List<Integer> array;
 
-    public Sort(SortingType sortingType) {
-        this.sortingType = sortingType;
+    public Sort(int sortingType, List<Integer> array) {
+        switch (sortingType) {
+            case 1 -> this.sortingType = SortingType.INCREASING_BY_VALUE;
+            case 2 -> this.sortingType = SortingType.DECREASING_BY_VALUE;
+            case 3 -> this.sortingType = SortingType.INCREASING_BY_LENGTH;
+            case 4 -> this.sortingType = SortingType.DECREASING_BY_LENGTH;
+        }
+        this.array = array;
     }
 
-    public void run(List<Integer> array) {
+    @Override
+    public synchronized void start() {
         switch (sortingType) {
             case INCREASING_BY_VALUE -> Collections.sort(array);
             case DECREASING_BY_VALUE -> array.sort(Comparator.reverseOrder());
-            case INCREASING_BY_LENGTH -> array.sort(new CompareByLength());
-            case DECREASING_BY_LENGTH -> array.sort(new CompareByLength().reversed());
+            case INCREASING_BY_LENGTH -> array.sort((Comparator.comparingInt(o -> o.toString().length())));
+            case DECREASING_BY_LENGTH -> array.sort(((o1, o2) -> o2.toString().length() - o1.toString().length()));
         }
     }
 }
